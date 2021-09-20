b67baaadb87b3bc2cbe7bebc99aa299518456ba4
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-20 09:12:24 +0530

    Update vcbot.py

--- SaberUB/modules/vcbot.py
+++ SaberUB/modules/vcbot.py
@@ -80,7 +80,112 @@ async def _(e):
         await eor(e, f"`{str(ex)}`")
 
 
-@SaberUB_cmd(
+"""
+✘ Commands Available -
+• `{i}startvc`
+    Start Group Call in a group.
+• `{i}stopvc`
+    Stop Group Call in a group.
+• `{i}playvc`
+    Start Voice Chat Bot to receive Commands.
+• `{i}vcinvite`
+    Invite all members of group in Group Call.
+    (You must be joined)
+• `{i}vcaccess <id/username/reply to msg>`
+    Give access of Voice Chat Bot.
+• `{i}rmvcaccess <id/username/reply to msg>`
+    Remove access of Voice Chat Bot.
+• **Voice Chat Bot Commands**
+   `/play ytsearch : song-name`
+   `/play youtube link`
+   `/current`
+   `/skip`
+   `/exitVc`
+"""
+
+from pyUltroid.functions.vc_sudos import add_vcsudo, del_vcsudo, get_vcsudos, is_vcsudo
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from . import *
+
+
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call))
+    return xx.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+@ultroid_cmd(
+    pattern="stopvc$",
+    admins_only=True,
+    groups_only=True,
+)
+async def _(e):
+    try:
+        await e.client(stopvc(await get_call(e)))
+        await eor(e, "`Voice Chat Stopped...`")
+        vcdyno("off")
+    except Exception as ex:
+        await eor(e, f"`{str(ex)}`")
+
+
+@ultroid_cmd(
+    pattern="playvc$",
+)
+async def _(e):
+    zz = await eor(e, "`VC bot started...`")
+    er, out = await bash("npm start")
+    LOGS.info(er)
+    LOGS.info(out)
+    vcdyno("on")
+    if er:
+        await zz.edit(f"Failed {er}\n\n{out}")
+
+
+@ultroid_cmd(
+    pattern="vcinvite$",
+    groups_only=True,
+)
+async def _(e):
+    ok = await eor(e, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in e.client.iter_participants(e.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await e.client(invitetovc(call=await get_call(e), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await ok.edit(f"`Invited {z} users`")
+
+
+@ultroid_cmd(
+    pattern="startvc$",
+    admins_only=True,
+    groups_only=True,
+)
+async def _(e):
+    try:
+        await e.client(startvc(e.chat_id))
+        await eor(e, "`Voice Chat Started...`")
+    except Exception as ex:
+        await eor(e, f"`{str(ex)}`")
+
+
+@ultroid_cmd(
     pattern="listvcaccess$",
 )
 async def _(e):
@@ -97,7 +202,7 @@ async def _(e):
     await xx.edit(pp)
 
 
-@SaberUB_cmd(
+@ultroid_cmd(
     pattern="rmvcaccess ?(.*)",
 )
 async def _(e):
@@ -131,7 +236,7 @@ async def _(e):
         return await eod(xx, f"`{str(ex)}`", time=5)
 
 
-@SaberUB_cmd(
+@ultroid_cmd(
     pattern="vcaccess ?(.*)",
 )
 async def _(e):
