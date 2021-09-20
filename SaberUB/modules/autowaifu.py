1f31509772a7a6708841fa4ee9c7f9d393e27ba7
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 10:49:34 +0700

    Create autowaifu.py

---
+++ SaberUB/modules/autowaifu.py
@@ -0,0 +1,33 @@
+import time
+from pyrogram import filters
+import os
+from bs4 import BeautifulSoup
+import requests
+from SaberUB import app as Waifu, BOT_LIST
+
+@Waifu.on_message()
+async def autowaifu(client, message):
+    if message.photo:
+
+        if message.user.id in BOT_LIST:
+            dl = await Waifu.download_media(message, "resources/")
+            file = {"encoded_image": (dl, open(dl, "rb"))}
+            grs = requests.post(
+                "https://www.google.com/searchbyimage/upload",
+                files=file,
+                allow_redirects=False,
+            )
+            loc = grs.headers.get("location")
+            response = requests.get(
+                loc,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+                },
+            )
+            xx = bs(response.text, "html.parser")
+            div = xx.find_all("div", {"class": "r5a77d"})[0]
+            alls = div.find("a")
+            text = alls.text
+            send = await Waifu.send_message(message.chat.id, f"/protecc {text}")
+            await sleep(5)
+            os.remove(dl)
