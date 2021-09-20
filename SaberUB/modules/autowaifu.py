e7a6a0710aa3c785096a2ba9696853c7dde715ec
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 11:33:02 +0700

    maybe done

--- SaberUB/modules/autowaifu.py
+++ SaberUB/modules/autowaifu.py
@@ -4,7 +4,8 @@ import os
 from bs4 import BeautifulSoup
 import requests
 from SaberUB import app as Waifu, BOT_LIST, CMD_HELP
-
+from SaberUB.database.autoproteccdb import add_chat, is_chat_in_db, rm_chat
+from config import PREFIX
 
 CMD_HELP.update(
     {
@@ -17,12 +18,40 @@ CMD_HELP.update(
 )
 
 
-@Waifu.on_message()
+@app.on_message(filters.command("addharem", PREFIX) & filters.me)
+async def add_harem_hc(client, message):
+    pablo = await message.edit("`Processing..`")
+    if await is_chat_in_db(int(message.chat.id)):
+        await pablo.edit("`This Chat is Already In My DB`")
+        return
+    await add_chat(int(message.chat.id))
+    await pablo.edit("`Successfully Added Chat To Harem Watch.`")
+
+
+@app.on_message(filters.command("rmharem", PREFIX) & filters.me)
+async def remove_nsfw(client, message):
+    pablo = await message.edit("`Processing..`")
+    if not await is_chat_in_db(int(message.chat.id)):
+        await pablo.edit("`This Chat is Not in dB.`")
+        return
+    await rm_chat(int(message.chat.id))
+    await pablo.edit("`Successfully Removed Chat From Harem Watch.`")
+
+
+async def is_harem_enabled(f, client, message):
+    if await is_chat_in_db(int(message.chat.id)):
+        return
+    else:
+        return
+
+is_harem_enabled = filters.create(func=is_harem_enabled, name="is_harem_enabled")
+
+@app.on.message(filters.user([BOT_LIST]) & ~filters.edited & is_harem_enabled & filters.group)
 async def autowaifu(client, message):
     if message.photo:
 
         if message.user.id in BOT_LIST:
-            dl = await Waifu.download_media(message, "resources/")
+            dl = await app.download_media(message, "resources/")
             file = {"encoded_image": (dl, open(dl, "rb"))}
             grs = requests.post(
                 "https://www.google.com/searchbyimage/upload",
@@ -40,6 +69,6 @@ async def autowaifu(client, message):
             div = xx.find_all("div", {"class": "r5a77d"})[0]
             alls = div.find("a")
             text = alls.text
-            send = await Waifu.send_message(message.chat.id, f"/protecc {text}")
+            send = await app.send_message(message.chat.id, f"/protecc {text}")
             await sleep(5)
             os.remove(dl)
