478ab712cc609d574eb93d03498b3867bcf1e4d0
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-09-20 10:55:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ PREFIX = os.getenv("PREFIX")
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
 HEROKU_API = os.getenv("HEROKU_API")
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
-
+BOT_LIST = {int(x) for x in os.getenv("BOT_LIST", "").split()}
