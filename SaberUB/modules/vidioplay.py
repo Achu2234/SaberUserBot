0a767af1c764ef6259f1eacb7dde86d27b9c96d4
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-20 09:45:20 +0530

    hm

--- SaberUB/modules/vidioplay.py
+++ SaberUB/modules/vidioplay.py
@@ -3,9 +3,9 @@ import asyncio
 from pytgcalls import GroupCallFactory
 from pyrogram import filters, Client, idle
 from pyrogram.types import Message
-from config import API_ID, API_HASH, SESSION_NAME
+from config import API_ID, API_HASH, SESSION
 
-app = Client(SESSION_NAME, API_ID, API_HASH)
+app = Client(SESSION, API_ID, API_HASH)
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
 VIDEO_CALL = {}
 
