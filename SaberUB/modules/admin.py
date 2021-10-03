a405d8f556c3e16b5b0fc5046a4b448d8d53f274
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-10-03 14:17:07 +0530

    fixed

--- SaberUB/modules/admin.py
+++ SaberUB/modules/admin.py
@@ -105,20 +105,7 @@ async def mute_hammer(_, message: Message):
 
 
 # Unmute permissions
-unmute_permissions = ChatPermissions(
-    can_send_messages=True,
-    can_send_media_messages=True,
-    can_send_stickers=True,
-    can_send_animations=True,
-    can_send_games=True,
-    can_use_inline_bots=True,
-    can_add_web_page_previews=True,
-    can_send_polls=True,
-    can_change_info=False,
-    can_invite_users=True,
-    can_pin_messages=False,
-)
-
+    await bot.restrict_chat_member( query.message.chat.id, user_id, ChatPermissions( can_send_messages=True, can_send_media_messages=True, can_send_stickers=True, can_send_animations=True, can_send_games=True, can_use_inline_bots=True, can_add_web_page_previews=True, can_send_polls=True, can_change_info=False, can_invite_users=True, can_pin_messages=False ) )
 
 @app.on_message(filters.command("unmute", PREFIX) & filters.me)
 async def unmute(_, message: Message):
